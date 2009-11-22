7f86f9c1f860e268b1024e40572f58f3c4e2fb26
Riccardo Gori <goriccardo@gmail.com>
2009-11-22 19:13:50 +0100

    add CL calculation

--- wing2d.f90
+++ wing2d.f90
@@ -6,7 +6,8 @@
 !A circle in a _potential_ flow
 PROGRAM wing2d
       IMPLICIT NONE
-      integer, parameter :: Nelem = 9
+      integer, parameter :: Nelem = 19
+      integer, parameter :: NTime = 10
       integer :: i
 !     Vector of nodes global coordinates (x,y)
       real(kind=8), dimension(NELEM,2) :: Xnode
@@ -15,29 +16,35 @@ PROGRAM wing2d
       real(kind=8) :: uscalar = 1.
       real(kind=8) :: alpha = 6.
       real(kind=8), dimension(2) :: U
+      real(kind=8), dimension(NTime,2) :: Ut
       real(kind=8) :: Thick = 0.3
-      integer, parameter :: NTime = 10
 !     Potential and normal wash on the surface
       real(kind=8), dimension(Nelem,Nelem) :: B, C
       real(kind=8), dimension(NTime,2) :: XWnode
       real(kind=8), dimension(Nelem, Ntime) :: Chit, Phit, D
       real(kind=8), dimension(NTime) :: DPHIW
+      real(kind=8), dimension(NTime) :: cl
+      logical :: TEat1 = .true.
 !     Time step
       real(kind=8) :: DT, CalcDT
 !     The program starts here!
-      CALL BodyRotation(Uscalar, alpha, U)
-      CALL GeomWing(Nelem, Xnode, Chord, Thick)
+      call BodyRotation(Uscalar, alpha, U)
+      Ut(:,1) = U(1)
+      Ut(:,2) = U(2)
+      call GeomWing(Nelem, Xnode, Chord, Thick)
       DT = CalcDT(Nelem, Xnode, Uscalar)
-      CALL WakeGrid(Nelem, Xnode, Uscalar, DT, NTime, XWnode)
-      CALL SrfMatBCD(Nelem, Xnode, NTime, XWnode, B, C, D)
+      call WakeGrid(Nelem, Xnode, Uscalar, DT, NTime, XWnode)
+      call SrfMatBCD(Nelem, Xnode, NTime, XWnode, B, C, D)
       do i = 1,NTime
-       CALL BCondVel(Nelem, XNODE, U, Chit(:,i))
+       call BCondVel(Nelem, XNODE, U, Chit(:,i))
       end do
-      CALL SolvePhiTime(Nelem, B, C, NTime, D, Chit, Phit, DPhiW)
+      call SolvePhiTime(Nelem, B, C, NTime, D, Chit, Phit, DPhiW)
+      call CalcCl(Nelem, Xnode, TEat1, NTime, DT, Ut, Phit, Chit, cl)
+      write(*,*) cl
 !     Save results
-      CALL SavePhi(Nelem, NTime, PhiT)
-      CALL SaveXWnode(NTime, XWNODE)
-      CALL SaveDPhiW(NTime, DPHIW)
+      call SavePhi(Nelem, NTime, PhiT)
+      call SaveXWnode(NTime, XWNODE)
+      call SaveDPhiW(NTime, DPHIW)
 END PROGRAM
 
 
