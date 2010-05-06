fd74b4c7842ef915d60da7ec70ec02f150809ce6
Riccardo Gori <goriccardo@gmail.com>
2010-05-06 18:07:22 +0200

    Implemented theodorsen functions

--- etest.f90
+++ etest.f90
@@ -7,9 +7,8 @@ PROGRAM etest
       integer, parameter :: Nup = 100
       integer, parameter :: Nelem = Nup*2+1
       integer, parameter :: NWake = Nup*10
-      integer, parameter :: Nlen = 100
       real(kind=8), dimension(Nelem, 2) :: Xnode
-      real(kind=8), parameter :: rt = 6.D-2, chord = 1.D0, alpha = 0.D0, L = 2.5D0
+      real(kind=8), parameter :: rt = 6.D-2, chord = 1.D0, alpha = 0.D0, L = 1.0D0
       real(kind=8), parameter :: PI = 4.D0*datan(1.D0)
       real(kind=8), dimension(2,2) :: M, K
       real(kind=8) :: thick = chord*rt, vf
@@ -23,7 +22,7 @@ PROGRAM etest
       E(:,:) = 0
       call GeomNACA00xx(Nup, Chord, Thick, Xnode)
       !call EMatrixWing2d(Nelem, Xnode, alpha, NWake, p, E)
-      call EMatrixWing3DA(Nelem, Xnode, alpha, Nlen, L, Nwake, p, E)
+      call EMatrixWing3DA(Nelem, Xnode, alpha, L, Nwake, p, E)
       call theodorsen(p, L, Et)
       write(*,*) "BEM:"
       do i = 1,2
@@ -52,30 +51,3 @@ PROGRAM etest
 1001 FORMAT('',100(F15.8))
 END PROGRAM
 
-subroutine theodorsen(pbem, L, E)
-      implicit none
-      complex(kind=8), intent(in) :: pbem
-      complex(kind=8), dimension(2,2), intent(out) :: E
-      real(kind=8) :: k, L
-      complex(kind=8) :: c, p
-      real(kind=8), parameter :: PI = 4.D0*datan(1.D0)
-      complex(kind=8), parameter :: j = dcmplx(0.D0,1.D0)
-      p = pbem/2.D0
-      k = imagpart(p)
-      E(1,1) = 0.5D0*(k**2/5.D0 - 0.4D0*j*k*c(k))
-      E(1,2) = (-k*(k/8.D0 - j/4.D0) + c(k)*0.5D0*(1.D0+j*k))
-      E(2,1) = 0.5D0*(-c(k)*j*k/4.D0)
-      E(2,2) = (k*(k/24.D0-j/6.D0) + c(k)/3.D0*(1.D0+j*k))
-      E = 2.D0*PI*L*E
-end subroutine
-
-
-function c(k)
-      implicit none
-      complex(kind=8) :: c
-      real(kind=8) :: k
-      complex(kind=8) :: jk
-      jk = dcmplx(0.D0,k)
-      c = 0.5D0*(jk+0.135D0)*(jk+0.651D0)/((jk+0.0956D0)*(jk+0.4555D0))
-      return
-end function
--- theo.f90
+++ theo.f90
@@ -1,22 +1,36 @@
-
-subroutine theodorsen(pbem, E)
+subroutine theodorsen(pbem, L, E)
       implicit none
       complex(kind=8), intent(in) :: pbem
       complex(kind=8), dimension(2,2), intent(out) :: E
-      real(kind=8) :: k
+      real(kind=8) :: k, L
       complex(kind=8) :: c, p
       real(kind=8), parameter :: PI = 4.D0*datan(1.D0)
       complex(kind=8), parameter :: j = dcmplx(0.D0,1.D0)
       p = pbem/2.D0
-      k = cdabs(p)
-      E(1,1) = 2.D0*pi*(k**2/5.D0 - 0.4D0*j*k*c(k))
-      !E(1,1) = PI*2.D0*(p*(0.4D0*c(k) + p*0.2D0)) !Moltiplicata per 2 PI
-      E(1,2) = pi*(-k*(k/8.D0 - j/4.D0) + c(k)*0.5D0*(1.D0+j*k))
-      !E(1,2) = -0.5D0*PI*c(k) + p*(-0.25D0-c(k)/2.D0 - 0.125D0*p) !Moltiplicata per PI
-      E(2,1) = pi*(-c(k)*j*k/4.D0)
-      !E(2,1) = PI*(p*0.25D0*c(k)) !Moltiplicata per PI
-      E(2,2) = pi/2.D0*(k*(k/24.D0-j/6.D0) + c(k)/3.D0*(1.D0+j*k))
-      !E(2,2) = -0.5D0*PI/3.D0*c(k) + p*(1.D0/6.D0 - c(k) + 1.D0/24.D0*p) !Moltiplicata per PI/2
+      k = imagpart(p)
+      E(1,1) = 0.5D0*(k**2/5.D0 - 0.4D0*j*k*c(k))
+      E(1,2) = (-k*(k/8.D0 - j/4.D0) + c(k)*0.5D0*(1.D0+j*k))
+      E(2,1) = 0.5D0*(-c(k)*j*k/4.D0)
+      E(2,2) = (k*(k/24.D0-j/6.D0) + c(k)/3.D0*(1.D0+j*k))
+      E = 2.D0*PI*L*E
+end subroutine
+
+
+subroutine theodorsenST(pbem, L, E)
+      implicit none
+      complex(kind=8), intent(in) :: pbem
+      complex(kind=8), dimension(2,2), intent(out) :: E
+      real(kind=8) :: k, L
+      complex(kind=8) :: c, p
+      real(kind=8), parameter :: PI = 4.D0*datan(1.D0)
+      complex(kind=8), parameter :: j = dcmplx(0.D0,1.D0)
+      p = pbem/2.D0
+      k = imagpart(p)
+      E(1,1) = 0.5D0*(pi*k**2/32.D0 - pi*j*k*c(k)/16.D0)
+      E(1,2) = ( 2.D0/15.D0*(-k*(k/2.D0 - j)) + c(k)*4.D0/15.D0*(1.D0+j*k) )
+      E(2,1) = 0.5D0*(-c(k)*j*k*2.D0/15.D0)
+      E(2,2) = (pi/32.D0*k*(k/4.D0 - j) + c(k)*pi/16.D0*(1.D0+j*k))
+      E = 2.D0*PI*L*E
 end subroutine
 
 
